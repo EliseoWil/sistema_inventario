4a12050cc35374c3780dff0fac21f49ef64830f7
amaru <amaru.el.trueno@gmail.com>
2023-03-04 14:04:02 -0400

    04/03/23 avance 2 cliente

--- controlador/plantillaControlador.php
+++ controlador/plantillaControlador.php
@@ -1,7 +1,11 @@
 <?php
 
-class ControladorPlantilla {
-    static public function ctrPlantilla(){
-        include "vista/plantilla.php";
-    }
+class ControladorPlantilla
+{
+
+  static public function ctrPlantilla()
+  {
+
+    include "vista/plantilla.php";
+  }
 }
--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 /* controladores */
 require_once "controlador/usuarioControlador.php";
+require_once "controlador/clienteControlador.php";
+require_once "controlador/productoControlador.php";
 require_once "controlador/plantillaControlador.php";
 /* modelos */
 require_once "modelo/usuarioModelo.php";
+require_once "modelo/clienteModelo.php";
+require_once "modelo/productoModelo.php";
 
 $plantilla = new ControladorPlantilla();
 $plantilla->ctrPlantilla();
